093fbb9a27b8ee3f3f1ed151f1de2d131ce1c6a2
Suraksha-Rajagopalan <surakshagopalan@gmail.com>
2022-08-09 18:21:15 +0530

    Delete java directory

--- java/InputValues.java
+++
@@ -1,9 +0,0 @@
-import java.util.Scanner;
-public class InputValues {
-    public static void main(String[] args) {
-        System.out.println("Enter:");
-        Scanner scan = new Scanner(System.in);
-        String input = scan.nextLine();
-        System.out.println("The input string is :"+input);
-    }
-}
--- java/new.java
+++
@@ -1,7 +0,0 @@
-public class new
-{
-    public static void main(String args[])
-    {
-         System.out.print("Code");
-    }
-}
